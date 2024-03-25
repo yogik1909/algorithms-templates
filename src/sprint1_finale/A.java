19330f8f16d23b77207b99df877dc23a3e9df771
Beloshovec Vladimir <Beloshovec.VA@dns-shop.ru>
2024-03-26 00:26:21 +1000

    Save and solushion sprint 1 final A

---
+++ src/sprint1_finale/A.java
@@ -0,0 +1,59 @@
+package sprint1_finale;
+
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class A {
+
+    private static List<Integer> getNeighb(List<Integer> numberList, int k) {
+
+        List<Integer> neighb = new ArrayList<Integer>(Collections.nCopies(k, 0));
+        boolean EmptyFound = false;
+        int moveRigt = 1;
+        for (int ltIns = 0; ltIns < numberList.size(); ltIns++) {
+            if (numberList.get(ltIns) != 0) {
+                if (EmptyFound) neighb.set(ltIns, moveRigt++);
+                continue;
+            }
+            EmptyFound = true;
+
+
+            for (int j = 1;
+                 ltIns - j >= 0
+                         && numberList.get(ltIns - j) != 0
+                         && (neighb.get(ltIns - j) == 0 || j < neighb.get(ltIns - j));
+                    j++)
+            neighb.set(ltIns - j, j);
+            moveRigt = 1;
+
+        }
+
+
+
+
+        return neighb;
+    }
+    public static void main(String[] args) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            int numberOfHouses = readInt(reader);
+            List<Integer> addressSpace = readList(reader);
+            List<Integer> neighb = getNeighb(addressSpace, numberOfHouses);
+            for (int elem : neighb) {
+                writer.write( elem + " ");
+            }
+        }
+    }
+
+    private static int readInt(BufferedReader reader) throws IOException {
+        return Integer.parseInt(reader.readLine());
+    }
+
+    private static List<Integer> readList(BufferedReader reader) throws IOException {
+        return Arrays.asList(reader.readLine().split(" "))
+                .stream()
+                .map(elem -> Integer.parseInt(elem))
+                .collect(Collectors.toList());
+    }
+}
