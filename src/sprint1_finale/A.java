9ff2571bb669b7828b43c6a68e3b50d17a8bba61
Beloshovec Vladimir <Beloshovec.VA@dns-shop.ru>
2024-03-26 23:33:57 +1000

    Save and solushion sprint 1 final A 2

--- src/sprint1_finale/A.java
+++ src/sprint1_finale/A.java
@@ -7,28 +7,36 @@ import java.util.stream.Collectors;
 public class A {
 
     private static List<Integer> getNeighb(List<Integer> numberList, int k) {
-
-        List<Integer> neighb = new ArrayList<Integer>(Collections.nCopies(k, 0));
-        boolean EmptyFound = false;
-        int moveRigt = 1;
-        for (int ltIns = 0; ltIns < numberList.size(); ltIns++) {
-            if (numberList.get(ltIns) != 0) {
-                if (EmptyFound) neighb.set(ltIns, moveRigt++);
-                continue;
-            }
-            EmptyFound = true;
-
-
-            for (int j = 1;
-                 ltIns - j >= 0
-                         && numberList.get(ltIns - j) != 0
-                         && (neighb.get(ltIns - j) == 0 || j < neighb.get(ltIns - j));
-                    j++)
-            neighb.set(ltIns - j, j);
-            moveRigt = 1;
-
+        List<Integer> emptyIndexes = new LinkedList<>();
+        int empIndex = 0;
+        for (Integer val:numberList){
+            if(val == 0){emptyIndexes.add(empIndex);}
+            empIndex++;
         }
+        List<Integer> neighb = new LinkedList<Integer>(Collections.nCopies(k, Integer.valueOf(0)));
+        if (emptyIndexes.size() == neighb.size()
+            || emptyIndexes.size() == 0) return neighb;
 
+        int leftIndex, rightIndex;
+        for (int i = 1; i <= emptyIndexes.size()-1; i++){
+            leftIndex = emptyIndexes.get(i-1);
+            rightIndex = emptyIndexes.get(i);
+                int diff = rightIndex - leftIndex;
+                int div = diff / 2;
+                for (int offset = 1; offset <= div; offset++){
+                    neighb.set(leftIndex +offset, offset);
+                    neighb.set(rightIndex-offset, offset);
+                }
+        }
+        rightIndex = emptyIndexes.get(0);
+        for (int i = 0, rad = rightIndex; i < rightIndex ; i++, rad--) {
+            neighb.set(i, rad);
+        };
+        rightIndex = neighb.size()-1;
+        leftIndex = emptyIndexes.get(emptyIndexes.size()-1);
+        for (int i = leftIndex+1, rad = 1; i <= rightIndex ; i++, rad++) {
+            neighb.set(i, rad);
+        }
 
 
 
@@ -39,10 +47,13 @@ public class A {
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out))) {
             int numberOfHouses = readInt(reader);
             List<Integer> addressSpace = readList(reader);
+            //long startTime = System.currentTimeMillis();
             List<Integer> neighb = getNeighb(addressSpace, numberOfHouses);
+            //long time = System.currentTimeMillis() ;
             for (int elem : neighb) {
                 writer.write( elem + " ");
             }
+            //writer.write("Time:"+(time - startTime));
         }
     }
 
