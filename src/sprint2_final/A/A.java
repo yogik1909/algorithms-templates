4a2fb1d79bb7790492d44d85ef1d3a38c4324603
Beloshovec Vladimir <Beloshovec.VA@dns-shop.ru>
2024-04-03 09:43:52 +1000

    sprint 2 final A try 2

--- src/sprint2_final/A/A.java
+++ src/sprint2_final/A/A.java
@@ -12,6 +12,7 @@ class MyQueueSized {
     private int tail;
     private int max_n;
     private int size;
+
     public MyQueueSized(int max_size) {
         queue = new Integer[max_size];
         head = 0;
@@ -26,19 +27,18 @@ class MyQueueSized {
 
     public void push_back(int x) {
         if (size != max_n) {
-            queue[tail] = x;
             tail = (tail + 1) % max_n;
+            queue[tail] = x;
             size++;
-        }
-        else System.out.println("error");
+        } else System.out.println("error");
     }
+
     public void push_front(int x) {
         if (size != max_n) {
+            head = Math.floorMod((head - 1), max_n);
             queue[head] = x;
-            head = Math.floorMod((head - 1), max_n);//
             size++;
-        }
-        else System.out.println("error");
+        } else System.out.println("error");
     }
 
     public Integer pop_front() {
@@ -53,6 +53,7 @@ class MyQueueSized {
         System.out.println(x);
         return x;
     }
+
     public Integer pop_back() {
         if (isEmpty()) {
             System.out.println("error");
@@ -82,23 +83,25 @@ public class A {
                 m = p.matcher(reader.readLine());
                 m.find();
                 switch (m.group(1)) {
-                    case "push_back":{
+                    case "push_back": {
                         query.push_back(Integer.parseInt(m.group(2)));
                         break;
                     }
-                    case "push_front":{
+                    case "push_front": {
                         query.push_front(Integer.parseInt(m.group(2)));
                         break;
                     }
-                    case "pop_back":{
+                    case "pop_back": {
                         query.pop_back();
                         break;
                     }
-                    case "pop_front":{
+                    case "pop_front": {
                         query.pop_front();
                         break;
                     }
                 }
-            }}}
+            }
+        }
+    }
 }
 
