5161cdbe74b0ac98a64f3e516236e98205314d57
Vladimir Beloshovec <beloshovec.va@dns-shop.ru>
2024-04-06 21:22:43 +1000

    init 2 final B solve

--- src/sprint2_final/B/B.java
+++ src/sprint2_final/B/B.java
@@ -1,7 +1,52 @@
 package sprint2_final.B;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.Stack;
+
+
 public class B {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws IOException {
+
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+            Scanner sc = new Scanner(reader.readLine());
+
+            sc.useDelimiter(" ");
+            Stack<Integer> stack = new Stack<>();
+            while (sc.hasNext()){
+                if (sc.hasNextInt()){
+                    stack.push(sc.nextInt());
+                    continue;
+                }
+                switch (sc.next()) {
+                    case "+": {
+                        stack.push(stack.pop() + stack.pop());
+                        break;
+                    }
+                    case "*": {
+                        stack.push(stack.pop() * stack.pop());
+                        break;
+                    }
+                    case "/":{
+                        int divider = stack.pop();
+                        stack.push((int) Math.floor((double) stack.pop()  / (double) divider));
+                        break;
+                    }
+                    case "-":{
+                        int deductible = stack.pop();
+                        stack.push(stack.pop() - deductible);
+                        break;
+                    }
+
+                }
+
+            }
+        System.out.println(stack.pop());
+
+        }
 
     }
 }
