a15d87a357b0264cb9a02aa39b30f49be03e7014
Vladimir Beloshovec <beloshovec.va@dns-shop.ru>
2024-04-17 00:01:50 +1000

    sprint 3 H wrong

--- src/sprint3/H/H.java
+++ src/sprint3/H/H.java
@@ -4,42 +4,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.math.BigInteger;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
-import java.util.stream.Collectors;
 
 public class H {
     public static void main(String[] args) throws IOException {
         int n;
         String[] argz;
-        BigInteger[] bi;
-        LinkedList <Integer> res = new LinkedList<>();
+        LinkedList <String> res = new LinkedList<>();
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
             n = Integer.parseInt(reader.readLine());
-            bi = new BigInteger[n];
             argz = reader.readLine().split(" ");
             LinkedList<String> list = new LinkedList<>(Arrays.asList(argz));
+            //Collections.sort(list);
+
+
             AllCombinations(list, "", res);
-            System.out.println(Collections.max(res));
+            System.out.println(res.get(0));
         }
-
-
-
     }
 
-    static void AllCombinations(LinkedList<String> arr, String cur, LinkedList<Integer> resList){
+    static void AllCombinations(LinkedList<String> arr, String cur, LinkedList<String> resList){
         if (arr.size() == 0){
-//            System.out.println(cur);
-            resList.add(Integer.parseInt(cur));
+            resList.add(cur);
             return;
         }
-        for (int i = 0; i < arr.size() ; i++) {
-            LinkedList<String> newArr = (LinkedList<String>) arr.clone();
-            newArr.remove(i);
-            AllCombinations(newArr, cur + arr.get(i), resList);
-        }
-
+        Collections.sort(arr);
+        LinkedList<String> newArr = (LinkedList<String>) arr.clone();
+        newArr.remove(arr.size()-1);
+        AllCombinations(newArr, cur + arr.get(arr.size()-1), resList);
     }
 }
