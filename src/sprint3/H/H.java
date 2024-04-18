eb4e84ea2efa2e0b85c587110d6de44caded1cf4
Vladimir Beloshovec <beloshovec.va@dns-shop.ru>
2024-04-18 17:00:43 +1000

    sprint 3 h compare string

--- src/sprint3/H/H.java
+++ src/sprint3/H/H.java
@@ -3,36 +3,22 @@ package sprint3.H;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.LinkedList;
+
 
 public class H {
     public static void main(String[] args) throws IOException {
         int n;
         String[] argz;
-        LinkedList <String> res = new LinkedList<>();
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
             n = Integer.parseInt(reader.readLine());
             argz = reader.readLine().split(" ");
-            LinkedList<String> list = new LinkedList<>(Arrays.asList(argz));
-            Collections.sort(list);
-
-
-            AllCombinations(list, "", res);
-            System.out.println(res.get(0));
+            Arrays.sort(argz, (l, r) -> (r + l).compareTo(l + r));
+            StringBuilder ans = new StringBuilder();
+            for (String number : argz) {
+                ans.append(number);
+            }
+            System.out.println(ans);
         }
-    }
-
-    static void AllCombinations(LinkedList<String> arr, String cur, LinkedList<String> resList){
-        if (arr.size() == 0){
-            resList.add(cur);
-            return;
-        }
-        Collections.sort(arr);
-        LinkedList<String> newArr = (LinkedList<String>) arr.clone();
-        newArr.remove(arr.size()-1);
-        AllCombinations(newArr, cur + arr.get(arr.size()-1), resList);
     }
 }
