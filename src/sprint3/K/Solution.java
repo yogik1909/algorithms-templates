adfe20beb39eec0d2ce7bbaddd266bcca479246b
Vladimir Beloshovec <beloshovec.va@dns-shop.ru>
2024-04-23 22:16:40 +1000

    sprint 3 K solve

--- src/sprint3/K/Solution.java
+++ src/sprint3/K/Solution.java
@@ -6,7 +6,7 @@ public class Solution {
 		int cur_right = mid;
 		int to = 0;
 
-		int[] temp_arr = new int[arr.length];
+		int[] temp_arr = new int[right - left];
 
 		while (cur_left < mid && cur_right < right){
 			if (arr[cur_left] <= arr[cur_right]){
@@ -33,8 +33,8 @@ public class Solution {
 	}
 
 	public static void merge_sort(int[] arr, int left, int right) {
-		if (right - left > 2){
-			int mid = (right - left)>>1;
+		if (right - left >= 2){
+			int mid = (right + left)>>1;
 			merge_sort(arr, left, mid);
 			merge_sort(arr, mid , right);
 			merge(arr, left, mid , right);
@@ -43,10 +43,10 @@ public class Solution {
 	}
 
 	public static void main(String[] args) {
-//		int[] a = {1, 4, 9, 2, 10, 11};
-//		int[] b = merge(a, 0, 3, 6);
-//		int[] expected = {1, 2, 4, 9, 10, 11};
-//		assert Arrays.equals(b, expected);
+		int[] a = {1, 4, 9, 2, 10, 11};
+		int[] b = merge(a, 0, 3, 6);
+		int[] expected = {1, 2, 4, 9, 10, 11};
+		assert Arrays.equals(b, expected);
 		int[] c = {1, 4, 2, 10, 1, 2};
 		merge_sort(c, 0, 6);
 		int[] expected2 = {1, 1, 2, 2, 4, 10};
