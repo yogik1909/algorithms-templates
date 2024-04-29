86f6e4ef69994511d9bbeca72cf53a96a7743490
Vladimir Beloshovec <beloshovec.va@dns-shop.ru>
2024-04-29 12:32:20 +1000

    sprint 4 A try 1

--- src/sprint4/A/A.java
+++ src/sprint4/A/A.java
@@ -1,9 +1,36 @@
 package sprint4.A;
 
+import javax.sound.midi.Soundbank;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class A {
+    public static void main(String[] args) throws IOException {
+        int n;
+        String data;
+        Map<String, Integer> map = new Map<>();
+        Integer val;
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
+            n = Integer.parseInt(reader.readLine());
+            for (int i = 0; i < n; i++) {
+                data = reader.readLine();
+                 if ((val = map.get(data)) != null){
+                    map.set(data, val+1);
+                 }else {
+                     map.set(data, 1);
+                 };
+
+
+            }
+        }
+        for (Pair<String, Integer> item: map){
+            System.out.println(item.key);
+        }
+    }
 }
 class Pair<K, V> {
     K key;
@@ -14,7 +41,7 @@ class Pair<K, V> {
         this.value = value;
     }
 }
-class Map<K, V> {
+class Map<K, V> implements Iterable<Pair<K, V>> {
     private List<Pair<K, V>> pairs;
 
     public Map() {
@@ -40,5 +67,14 @@ class Map<K, V> {
         // Если пара с заданным ключом не найдена, добавим новую пару
         Pair<K, V> newPair = new Pair<>(key, value);
         pairs.add(newPair);
+    }
+
+    @Override
+    public Iterator<Pair<K, V>> iterator() {
+        return pairs.iterator();
+    }
+
+    public int getCountItem(){
+        return pairs.size();
     }
 }
