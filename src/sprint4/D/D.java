f42843b5bb4b41462a13d04b309525cf9deb3ddf
Vladimir Beloshovec <beloshovec.va@dns-shop.ru>
2024-05-09 00:57:35 +1000

    sprint 4 D solve in long

--- src/sprint4/D/D.java
+++ src/sprint4/D/D.java
@@ -3,13 +3,12 @@ package sprint4.D;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 public class D {
     public static void main(String[] args) throws IOException {
         int a, m;
         String s;
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+        try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
             a = Integer.parseInt(reader.readLine());
             m = Integer.parseInt(reader.readLine());
             s = reader.readLine();
@@ -22,13 +21,11 @@ public class D {
     }
 
     public static int Polynomial(String hasingString, int x, int mod) {
-        BigInteger res = new BigInteger(String.valueOf(0));
-        BigInteger mult = new BigInteger(String.valueOf(x));
-        BigInteger modVal = new BigInteger(String.valueOf(mod));
+    long res = 0;
 
-        for (int i = 0; i < hasingString.length(); i++) {
-            res = res.multiply(mult).add(BigInteger.valueOf(hasingString.charAt(i))).mod(modVal);
-        }
-        return Integer.parseInt(res.toString());
+        for (int i = 0; i < hasingString.length(); i++){
+            res = (res * x + hasingString.charAt(i)) % mod;
+    }
+    return (int) res;
     }
 }
