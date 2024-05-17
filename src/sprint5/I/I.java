b9c72256b0334791de59a2ae414cd8c24a5c70f6
Vladimir Beloshovec <beloshovec.va@dns-shop.ru>
2024-05-17 22:54:00 +1000

    sprint 5 I try 3 with Catlan namber

--- src/sprint5/I/I.java
+++ src/sprint5/I/I.java
@@ -3,73 +3,28 @@ package sprint5.I;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class I {
-    private static Map<String, List<Node>> cache = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
         int n;
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))){
             n = Integer.parseInt(reader.readLine());
         }
 
-        List<Node> listTreeNode = generateTrees(n);
-        System.out.println(listTreeNode.size());
+        System.out.println(CatlanNumber(n));
     }
 
-    public static List<Node> generateTrees(int n) {
-        if (n == 0) {
-            return new ArrayList<Node>();
-        }
-        return generateTrees(1, n);
-    }
+    private static int CatlanNumber(int n){
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
 
-    private static List<Node> generateTrees(int start, int end) {
-        String key = start + "," + end;
-        if (cache.containsKey(key)) {
-            return cache.get(key);
-        }
-        List<Node> allTrees = new ArrayList<>();
-        if (start > end) {
-            allTrees.add(null);
-            return allTrees;
-        }
-
-        for (int i = start; i <= end; i++) {
-            List<Node> leftTrees = generateTrees(start, i - 1);
-            List<Node> rightTrees = generateTrees(i + 1, end);
-
-            for (Node l : leftTrees) {
-                for (Node r : rightTrees) {
-                    Node currentTree = new Node(i);
-                    currentTree.left = l;
-                    currentTree.right = r;
-                    allTrees.add(currentTree);
-                }
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
             }
         }
-        cache.put(key, allTrees);
-        return allTrees;
-    } 
-    private static class Node {
-        int value;
-        Node left;
-        Node right;
 
-        Node(int value) {
-            this.value = value;
-            this.left = null;
-            this.right = null;
-        }
-
-        Node(int value, Node left, Node right) {
-            this.value = value;
-            this.left = left;
-            this.right = right;
-        }
+        return dp[n];
     }
-
 }
