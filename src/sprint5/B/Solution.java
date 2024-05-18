48ac182d9881f8fc111e40b2d40511ab9ca70e9b
Vladimir Beloshovec <beloshovec.va@dns-shop.ru>
2024-05-19 00:17:22 +1000

    sprint 5 B try 1

--- src/sprint5/B/Solution.java
+++ src/sprint5/B/Solution.java
@@ -1,7 +1,33 @@
+package sprint5.B;
+
 public class Solution {
+    public static void main(String[] args) {
+        Node node0 = new Node(0);
+        Node node1 = new Node(1);
+        Node node2 = new Node(2);
+        node1.left = node2;
+        node1.right = node0;
+        System.out.println(treeSolution(node1));
+    }
     public static boolean treeSolution(Node head) {
-        // Your code
-        // “ヽ(´▽｀)ノ”
+        return getChildHeightOrBalance(head) != -1;
+    }
+    private static int getChildHeightOrBalance(Node node) {
+        if (node == null) {
+            return 0; // Высота пустого дерева равна 0
+        }
+
+        int leftHeight = getChildHeightOrBalance(node.left);
+        if (leftHeight == -1) return -1; // Левое поддерево не сбалансировано
+
+        int rightHeight = getChildHeightOrBalance(node.right);
+        if (rightHeight == -1) return -1; // Правое поддерево не сбалансировано
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1; // Текущий узел не сбалансирован
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1; // Возвращаем высоту текущего узла
     }
 
     // <template>
