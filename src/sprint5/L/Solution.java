b9724c4c9d2d23578817826c58e3dcc484954225
Vladimir Beloshovec <beloshovec.va@dns-shop.ru>
2024-05-21 00:04:42 +1000

    sprint 5 L try 1

--- src/sprint5/L/Solution.java
+++ src/sprint5/L/Solution.java
@@ -1,7 +1,28 @@
+package sprint5.L;
+
 public class Solution {
+    public static void main(String[] args) {
+        int[] sample = {-1, 12, 1, 8, 3, 4, 7};
+        System.out.println(siftDown(sample, 2) == 5);
+    }
     public static int siftDown(int[] heap, int idx) {
-        // Your code
-        // “ヽ(´▽｀)ノ”
+        int idx_1 = idx - 1;
+        int indLeftChild = idx * 2 - 1;
+        int indRightChild = indLeftChild + 1;
+
+        if (indLeftChild >= heap.length - 1) {return idx;}
+
+        int indexLargest = indLeftChild;
+        if (indRightChild < heap.length - 1 && heap[indRightChild] > heap[indexLargest]) {
+            indexLargest = indRightChild;
+        }
+        if (heap[indexLargest] > heap[idx_1]) {
+            int temp = heap[idx_1];
+            heap[idx_1]= heap[indexLargest];
+            heap[indexLargest] = temp;
+            return siftDown(heap, indexLargest)+1;
+        }
+        return indexLargest+1;
     }
 
     private static void test() {
