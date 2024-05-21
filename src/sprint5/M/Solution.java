5839407147c4d1b4874b456d6d5ab4ed79a96151
Vladimir Beloshovec <beloshovec.va@dns-shop.ru>
2024-05-21 21:03:41 +1000

    sprint 5 M try 1

--- src/sprint5/M/Solution.java
+++ src/sprint5/M/Solution.java
@@ -1,7 +1,24 @@
+package sprint5.M;
+
 public class Solution {
+    public static void main(String[] args) {
+        int[] sample = {12, 6, 8, 3, 15, 7};
+        System.out.println(siftUp(sample, 5) == 1);
+    }
     public static int siftUp(int[] heap, int idx) {
-        // Your code
-        // “ヽ(´▽｀)ノ”
+        int curIndex = idx - 1;
+        if (idx == 1) {
+            return idx;
+        }
+
+        int indexParent = curIndex >> 1;
+        if (heap[indexParent] < heap[curIndex]) {
+            int temp = heap[curIndex];
+            heap[indexParent] = heap[curIndex];
+            heap[curIndex] = temp;
+            return siftUp(heap, indexParent + 1);
+        }
+        return curIndex + 1;
     }
 
     private static void test() {
