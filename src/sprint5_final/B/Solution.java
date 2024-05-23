2b21e9ceecf681968db15c4133fbc01d975a39d2
Vladimir Beloshovec <beloshovec.va@dns-shop.ru>
2024-05-23 21:58:08 +1000

    sprint 5 final B try 1

--- src/sprint5_final/B/Solution.java
+++ src/sprint5_final/B/Solution.java
@@ -39,6 +39,23 @@ class Node {
 // <template>
 
 public class Solution {
+    public static void main(String[] args) {
+        Node node1 = new Node(null, null, 2);
+        Node node2 = new Node(node1, null, 3);
+        Node node3 = new Node(null, node2, 1);
+        Node node4 = new Node(null, null, 6);
+        Node node5 = new Node(node4, null, 8);
+        Node node6 = new Node(node5, null, 10);
+        Node node7 = new Node(node3, node6, 5);
+        Node newHead = remove(node7, 10);
+
+        System.out.println( newHead.getValue() == 5);
+        System.out.println(  newHead.getRight() == node5);
+        System.out.println(  newHead.getRight().getValue() == 8);
+
+
+
+    }
     public static Node remove(Node root, int key) {
         if (root == null) {
             return root;
@@ -54,6 +71,24 @@ public class Solution {
         }
          //Начинается обработка узла с ключе  раным улючу удаления
         // Сначала обрабатываем первый случай узла без потомков
+        else {
+        if (root.getLeft() == null)
+            return root.getRight();
+        else if (root.getRight() == null)
+            return root.getLeft();
+
+        Node maxValLeftChild = root.getLeft();
+        while (maxValLeftChild != null){
+            root.setValue(maxValLeftChild.getValue());
+            maxValLeftChild = maxValLeftChild.getRight();
+        }
+        root.setLeft(remove(root.getLeft(), root.getValue()));
+
+        }
+
+
+
+
 
         return root;
     }
